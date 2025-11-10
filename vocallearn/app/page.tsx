3666c60b1915b3f0cb6087e427d19ff825cc53b0
SaqiKaisar <saqifkaisar@iut-dhaka.edu>
2025-11-10 12:20:21 +0600

    Navbar created

--- vocallearn/app/page.tsx
+++ vocallearn/app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <h1 className="text-2xl underline">Welcome to my SAAS App</h1>
+      <Button>Let's get started</Button>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
