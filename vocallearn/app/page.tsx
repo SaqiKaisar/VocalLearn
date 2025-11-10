6c838b6c1d0a4cdf94d2469e4d18099bb52ab0b6
SaqiKaisar <saqifkaisar@iut-dhaka.edu>
2025-11-10 19:39:08 +0600

    Implement Components of the home page, 1:05:05

--- vocallearn/app/page.tsx
+++ vocallearn/app/page.tsx
@@ -1,11 +1,48 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SAAS App</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          subject="science"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="2"
+          subject="maths"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          duration={30}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="3"
+          subject="language"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
