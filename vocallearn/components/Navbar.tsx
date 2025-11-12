e7454fcc60aecfed874b49e082d12b40591aec23
SaqiKaisar <saqifkaisar@iut-dhaka.edu>
2025-11-12 18:25:52 +0600

    Implement Clerk authentication 1:30:50

--- vocallearn/components/Navbar.tsx
+++ vocallearn/components/Navbar.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import NavItems from "./NavItems";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -12,7 +19,16 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <div className="flex gap-3">
+            <SignInButton>
+              <button className="btn-signin hover:scale-102">Sign In</button>
+            </SignInButton>
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
