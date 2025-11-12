9ddd144e34714ffa5e6a5f9229835344cb7622e5
SaqiKaisar <saqifkaisar@iut-dhaka.edu>
2025-11-12 19:55:58 +0600

    Implement create companion component(database and onSubmit). 1:58:26

--- vocallearn/components/CompanionForm.tsx
+++ vocallearn/components/CompanionForm.tsx
@@ -26,6 +26,8 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
+import { createCompanion } from "@/lib/actions/companions.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -61,8 +63,14 @@ const CompanionForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const companion = await createCompanion(values);
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion");
+      redirect("/");
+    }
   };
 
   return (
